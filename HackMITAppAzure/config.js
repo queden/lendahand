c7bd2d2d3c3fafdb0b19091dec264cead2bf646f
queden <cadenmfelton@gmail.com>
2019-09-16 09:04:03 -0500

    Privacy Edits

--- HackMITAppAzure/config.js
+++ HackMITAppAzure/config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.endpoint = 'https://d73c6341-0ee0-4-231-b9ee.documents.azure.com:443/'
-config.key = 'mkLH4YF8cKgrKsoc3wteAOWNeExerwIcZoIXDZ4XHIXlpnAIC5ljiYEHSKv6zoU47bi0XOyggUnXveCXe5NmtA=='
+config.endpoint = '[COSMOS ENDPOINT]'
+config.key = '[COSMOS KEY]'
 
 config.database = {
   id: 'EventDatabase'
